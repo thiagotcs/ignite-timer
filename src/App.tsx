7dcf3ede6de4a9e086d11b628b587a2195c10a0d
Thiago Carvalho Da Silva <x470002@gruposantander.com>
2022-08-20 23:37:05 -0300

    configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./components/styles/themes/default";
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="success" />
+        <Button variant="danger" />
+        <Button />
+      </ThemeProvider>
+    </>
   );
 };
